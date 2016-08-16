c188381e0d7051c172b7e192a1738162a2c43d49
cody <codyberlin@gmail.com>
2016-08-16 14:46:40 -0700

    added project styling to the projects section

--- js/control.js
+++ js/control.js
@@ -1,6 +1,24 @@
 $('#where-i-am').on('click', function(){
+  hideEverything();
   $('#location-header').css('display', 'flex');
   $('#location-map').show();
   initMap();
-
 });
+
+$('#projects').on('click', function(){
+  hideEverything();
+  $('.project-image-container').css('display', 'flex');
+});
+$('.project-image-container').hover(function(){
+  $(this).children(':first').css('-webkit-filter', 'grayscale(1)');
+},
+function(){
+  $(this).children().css('-webkit-filter', 'grayscale(0)');
+}
+);
+
+function hideEverything(){
+  $('.project-image-container').css('display', 'none');
+  $('#location-map').hide();
+  $('#location-header').css('display', 'none');
+}
