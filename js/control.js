0f489b3a4cdbdd27a8abc060382921379fbd7fdf
cgberlin <codyberlin@gmail.com>
2016-08-16 23:17:25 -0700

    added contact page

--- js/control.js
+++ js/control.js
@@ -9,16 +9,38 @@ $('#projects').on('click', function(){
   hideEverything();
   $('.project-image-container').css('display', 'flex');
 });
+
+$('#contact').on('click', function(){
+  hideEverything();
+  $('#contact-me').css('display', 'flex');
+});
+
+
+
+
 $('.project-image-container').hover(function(){
   $(this).children(':first').css('-webkit-filter', 'grayscale(1)');
+  $(this).children().show(400);
 },
 function(){
   $(this).children().css('-webkit-filter', 'grayscale(0)');
+  $(this).children(':not(:first-child)').hide(400);
 }
 );
+
+$('.contact-buttons').hover(function(){
+  $(this).animate({width:'18%'}, 300);
+},
+function(){
+  $(this).animate({width:'15%'}, 300);
+}
+);
+
+
 
 function hideEverything(){
   $('.project-image-container').css('display', 'none');
   $('#location-map').hide();
   $('#location-header').css('display', 'none');
+  $('#contact-me').hide();
 }
